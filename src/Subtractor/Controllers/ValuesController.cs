c47b2683e1d31f948f3fff8665f85d5ec840a65a
Adrian Cheong <adriancheong@gic.com.sg>
2016-09-21 22:10:55 +0800

    Added Subtract method

--- src/Subtractor/Controllers/ValuesController.cs
+++ src/Subtractor/Controllers/ValuesController.cs
@@ -7,38 +7,13 @@ using Microsoft.AspNetCore.Mvc;
 namespace Subtractor.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class SubtractController : Controller
     {
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public int Get(int param1, int param2)
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return param1 - param2;
         }
     }
 }
